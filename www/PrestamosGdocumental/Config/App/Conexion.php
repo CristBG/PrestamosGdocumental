d6cb09a9854fe9090d4a93f7a22ba038c2838754
root <root@docker.docker>
2023-11-23 14:36:27 -0500

    Ajuste de base datos

--- www/PrestamosGdocumental/Config/App/Conexion.php
+++ www/PrestamosGdocumental/Config/App/Conexion.php
@@ -3,21 +3,16 @@ class Conexion{
     private $conect;
     public function __construct()
     {
-        $pdo = "mysql:host=".host.";dbname=".db.";.charset.";
+        $pdo = "mysql:host=".hostDB.";dbname=".db.";.charset.";
         try {
-                    echo '<script>console.log("inicio' . $pdo . '");</script>';
             $this->conect = new PDO($pdo, user, pass);
-                        echo '<script>console.log("2");</script>';
             $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo '<script>console.log("nice");</script>';
         } catch (PDOException $e) {
             echo "Error en la conexion".$e->getMessage();
-            echo '<script>console.log("Error en la conexion' . $e->getMessage() . '");</script>';
         }
     }
     public function conect()
     {
-                echo '<script>console.log("prueba");</script>';
         return $this->conect;
     }
 }
--- www/PrestamosGdocumental/Config/Config.php
+++ www/PrestamosGdocumental/Config/Config.php
@@ -1,6 +1,7 @@
 <?php
 const base_url = "http://localhost:80/PrestamosGdocumental/";
 const host = "localhost";
+const hostDB = "db";
 const user = "root";
 const pass = "rootpass";
 const db = "prestamos";
