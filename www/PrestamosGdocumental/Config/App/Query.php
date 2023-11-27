58b7ec29628e760c11662344dc1c5a1511723b25
cpardoan <142365655+cpardoan@users.noreply.github.com>
2023-11-27 08:19:28 -0500

    Update Query.php
    Se actualiza error de lectura json

--- www/PrestamosGdocumental/Config/App/Query.php
+++ www/PrestamosGdocumental/Config/App/Query.php
@@ -7,8 +7,7 @@ class Query extends Conexion{
     }
     public function select(string $sql)
     {
-        $this->sql = $sql;
-        echo '<script>console.log("select");</script>';
+        $this->sql = $sql;        
         $resul = $this->con->prepare($this->sql);
         $resul->execute();
         $data = $resul->fetch(PDO::FETCH_ASSOC);
